e004fc1d32637482ba82ef6df2557450f2cbeb40
slavker99 <48331491+slavker99@users.noreply.github.com>
2020-04-11 17:04:00 +0500

    updating repository

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-//router.get('/auth/:id', authUser);
-//router.post('/post/example', postExample)
-
-module.exports = router;
