a6a25ad7e0517e187cf6d71f1bcf2fdc3b797686
88ORLOV <44868289+88ORLOV@users.noreply.github.com>
2020-06-21 23:23:22 +0500

    corrections
    исправления

--- front/src/pages/Auth.js
+++ front/src/pages/Auth.js
@@ -27,9 +27,9 @@ class Auth extends React.Component {
   }
 
   handleClick() {
-    if (this.state.login.length == 0)
+    if (this.state.login.length === 0)
       this.setState({message: 'Введите логин!'});
-    else if (this.state.password.length == 0)
+    else if (this.state.password.length === 0)
       this.setState({message: 'Введите пароль!'});
     else 
       fetch('/api/auth', {
@@ -44,7 +44,7 @@ class Auth extends React.Component {
   }
 
   render() {
-    if (this.state.message == "Вы успешно авторизовались")
+    if (this.state.message === "Вы успешно авторизовались")
       return (
         <div>
           <label>{this.state.message}</label><br/> 
@@ -59,9 +59,9 @@ class Auth extends React.Component {
         <label>Логин</label><br/>
         <input type="text" value={this.state.login} onChange={this.handleLogin} /><br/><br/>
         <label>Пароль</label><br/>
-        <input type="text" value={this.state.password} onChange={this.handlePassword} /><br/>
+        <input type="password" value={this.state.password} onChange={this.handlePassword} /><br/>
         <label>{this.state.message}</label><br/>
-        <button onClick={this.handleClick}> Войти </button>
+        <button onClick={this.handleClick}>Войти</button>
       </div>
     );
   }
